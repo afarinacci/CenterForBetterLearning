1878619986b84e66db2f8f50de3b421dbcfca217
AFarinacci <andreafarinacci@me.com>
2020-02-02 16:57:40 +0700

    add custom fonts

--- src/App.js
+++ src/App.js
@@ -5,32 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App container">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Hello!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Center for Better Learning
-        </a>
-        <button type="button" className="btn btn-primary">
-          Click ME
-        </button>
-        <button type="button" className="btn btn-warning">
-          Click Me!
-        </button>
-        <button type="button" className="btn btn-success">
-          Click Me!
-        </button>
-        <button type="button" className="btn btn-danger">
-          Click Me!
-        </button>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="logotitle">Center for Better Learning</h1>
+      <h2 className="logotagline">
+        Better Vision. <span className="logoyellow">Brighter</span> Future.
+      </h2>
     </div>
   );
 }
