4365a155170c7535f81e3a1feed9f73b94957eb5
AFarinacci <andreafarinacci@me.com>
2020-02-04 19:28:17 +0700

    add footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 function Footer() {
   return (
@@ -6,32 +8,42 @@ function Footer() {
       <div className="row">
         <div className="location col-12 col-md-6 text-center">
           <h3>Coconut Creek</h3>
-          <address>
-            4147 Hillsboro Blvd Suite 13
-            <br />
-            Coconut Creek FL, 33703
-            <br />
-          </address>
-
+          <div>
+            <address>
+              <span>
+                <FontAwesomeIcon icon={faMapMarkerAlt} className="footerIcon" />
+              </span>
+              4147 Hillsboro Blvd Suite 13
+              <br />
+              Coconut Creek FL, 33703
+              <br />
+            </address>
+          </div>
           <a
             href="tel:+1561-264-1245"
             className="btn btn-outline-primary bg-white"
           >
+            <FontAwesomeIcon icon={faPhone} className="footerIcon" />
             (561) 264-1245
           </a>
         </div>
         <div className="location col-12 col-md-6 text-center">
           <h3>West Palm Beach</h3>
-          <address>
-            3650 Forest Hill Blvd. Suite 2<br />
-            West Palm Beach, FL 33406
-            <br />
-          </address>
-
+          <div>
+            <address>
+              <span>
+                <FontAwesomeIcon icon={faMapMarkerAlt} className="footerIcon" />
+              </span>
+              3650 Forest Hill Blvd. Suite 2<br />
+              West Palm Beach, FL 33406
+              <br />
+            </address>
+          </div>
           <a
             href="tel:+1561-264-2345"
             className="btn btn-outline-primary bg-white"
           >
+            <FontAwesomeIcon icon={faPhone} className="footerIcon" />
             (561) 264-2345
           </a>
         </div>
