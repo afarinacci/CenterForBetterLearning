e924fd6fa14aa7de192fc373803b0b46337a6aff
AFarinacci <andreafarinacci@me.com>
2020-02-04 17:02:13 +0700

    add reactstrap and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,41 @@
 import React from 'react';
 
 function Footer() {
-  return <div className="Footer">FOOTER</div>;
+  return (
+    <footer className="fixed-bottom row">
+      <div className="col-12 col-md-6 text-center">
+        <h3>Coconut Creek</h3>
+        <address>
+          4147 Hillsboro Blvd Suite 13
+          <br />
+          Coconut Creek FL, 33703
+          <br />
+        </address>
+
+        <a
+          href="tel:+1561-264-1245"
+          className="btn btn-outline-primary bg-white"
+        >
+          (561) 264-1245
+        </a>
+      </div>
+      <div className="col-12 col-md-6 text-center">
+        <h3>West Palm Beach</h3>
+        <address>
+          3650 Forest Hill Blvd. Suite 2<br />
+          West Palm Beach, FL 33406
+          <br />
+        </address>
+
+        <a
+          href="tel:+1561-264-2345"
+          className="btn btn-outline-primary bg-white"
+        >
+          (561) 264-2345
+        </a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
