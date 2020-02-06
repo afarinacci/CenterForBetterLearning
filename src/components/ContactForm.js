e55a6870b55cde8498abc9b02799b82198d017c0
AFarinacci <andreafarinacci@me.com>
2020-02-06 17:21:10 +0700

    add emailjs

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Col, Row, Form, FormGroup, Label, Input } from 'reactstrap';
-
+import emailjs from 'emailjs-com';
 class ContactForm extends React.Component {
   constructor(props) {
     super(props);
@@ -17,10 +17,32 @@ class ContactForm extends React.Component {
     });
   }
   handleSubmit(e) {
-    const userID = 'user_tMxXXkIPTSbBxvv05sm2e';
-    const template = 'CenterForBetterLearning';
     e.preventDefault();
-    console.log(this.state.name + ' ' + this.state.tel);
+    const userID = 'user_tMxXXkIPTSbBxvv05sm2e';
+    const templateID = 'CenterForBetterLearning';
+    const templateParams = {
+      to_name: 'afarinac.14@gmail.com',
+      from_name: this.state.name,
+      from_tel: this.state.tel,
+      reply_to: this.state.email
+      message_html: this.state.message,
+    };
+    
+    emailjs
+      .send(
+        'gmail',
+        templateID,
+        templateParams,
+        userID
+      )
+      .then(
+        response => {
+          console.log('SUCCESS!', response.status, response.text);
+        },
+        err => {
+          console.log('FAILED...', err);
+        }
+      );
     this.setState({ name: '', tel: '', email: '', message: '' });
   }
   render() {
