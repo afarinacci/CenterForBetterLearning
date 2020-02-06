d1290f1976739d000307f6d7da31ce8427d4913b
AFarinacci <andreafarinacci@me.com>
2020-02-06 11:11:02 +0700

    update hours

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -33,7 +33,7 @@ function Footer() {
           </div>
           <div className="col-12 col-lg-6 text-center ">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3576.242939625798!2d-80.18354878496756!3d26.318631783387072!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d91c7de52ad3b1%3A0xa72e0705bacc4e88!2s4171%20W%20Hillsboro%20Blvd%2C%20Coconut%20Creek%2C%20FL%2033073%2C%20USA!5e0!3m2!1sen!2s!4v1580895084881!5m2!1sen!2s"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3576.258638795866!2d-80.18277158496754!3d26.318123283387358!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d91c875ee26d09%3A0xb7deb2e84fa680a9!2s4147%20W%20Hillsboro%20Blvd%2C%20Coconut%20Creek%2C%20FL%2033073%2C%20USA!5e0!3m2!1sen!2s!4v1580954921167!5m2!1sen!2s"
               width="220"
               height="220"
               frameborder="0"
@@ -49,8 +49,6 @@ function Footer() {
             <h3>West Palm Beach</h3>
             <div>
               <address>
-                Located inside Premier Eye Center
-                <br />
                 <span>
                   <FontAwesomeIcon
                     icon={faMapMarkerAlt}
@@ -61,6 +59,9 @@ function Footer() {
                 <br />
                 West Palm Beach, FL 33406
                 <br />
+                <span style={{ fontStyle: 'italic' }}>
+                  Located inside Premier Eye Center
+                </span>
               </address>
             </div>
             <a href="tel:+1561-264-2345">
@@ -88,16 +89,24 @@ function Footer() {
         style={{ margin: '2px', border: '1px solid rgba(255,255,255, 0.5)' }}
       />
       <div className="row footerRow">
-        <div className="col-12 text-center">
-          <h5>OFFICE HOURS: </h5>
-        </div>
-        <div className="col-12 text-center">
-          Monday-Friday: 9AM-6PM
-          <br />
-          Saturday: 9AM-1PM
-          <br />
-          Sunday: <span style={{ fontStyle: 'italic' }}>Closed</span>
-          <br />
+        <div className="col-12 d-flex justify-content-center ">
+          <div className="hours">
+            <h5 style={{ fontWeight: 'bold' }}>OFFICE HOURS:</h5>
+            Mon: 9:00 AM - 6:00 PM
+            <br />
+            Tue: 9:00 AM - 6:00 PM
+            <br />
+            Wed: 9:00 AM - 6:00 PM
+            <br />
+            Thu: 9:00 AM - 6:00 PM
+            <br />
+            Fri: 9:00 AM - 6:00 PM
+            <br />
+            Sat: 9:00 AM - 1:00 PM
+            <br />
+            Sun: <span style={{ fontStyle: 'italic' }}>Closed</span>
+            <br />
+          </div>
         </div>
       </div>
     </footer>
