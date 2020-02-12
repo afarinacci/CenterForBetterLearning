d8daaa8670d608636374d91e61f11a222ef89761
AFarinacci <andreafarinacci@me.com>
2020-02-12 16:56:22 +0700

    add rows to bio page

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -3,13 +3,23 @@ import React from 'react';
 function Bio(props) {
   return (
     <div className="card">
-      <img className="bioimg card-img-top" src={props.url} alt={props.name} />
-      <div className="card-body">
-        <h3 className="card-title">{props.name}</h3>
-        <h5 className="card-title">{props.role}</h5>
-        {props.bio.map(item => (
-          <p className="card-text">{item}</p>
-        ))}
+      <div className="row no-gutters">
+        <div className="col-md-4 my-auto text-center">
+          <img
+            className="bioimg card-img-top"
+            src={props.url}
+            alt={props.name}
+          />
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <h3 className="card-title">{props.name}</h3>
+            <h5 className="card-title">{props.role}</h5>
+            {props.bio.map(item => (
+              <p className="card-text">{item}</p>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/pages/Team.js
+++ src/pages/Team.js
@@ -4,45 +4,40 @@ import Bio from '../components/Bio';
 const Team = () => (
   <main className="container-fluid paddingAround max800 effect8">
     <h1 className="text-center pageTitle">Meet Our Team</h1>
-    <div className="row">
-      <div className="col-md-6 col-lg-4">
-        <div className="biocard">
-          <Bio
-            name="Dr. Christina Murray, O.D."
-            bio={[
-              "Dr. Murray is originally from Columbus, Ohio, and is a proud graduate of The Ohio State University. She happily escaped the cold Ohio winters to attend optometry school at Nova Southeastern University. While in school, she discovered her passion for pediatrics and vision therapy. Dr. Murray's passion for helping children and adults reach their full potential through vision therapy has inspired her to Center for Better Learning. She is currently finishing up her board certification from the College of Optometrists in Vision Development.",
-              'When not seeing patients, Dr. Murray enjoys spending time with her husband, Paul, daughter, Mia, and two adorable golden retrievers, Sadie and Bentley. Her favorite activities include reading, baking, traveling.'
-            ]}
-            url="https://da4e1j5r7gw87.cloudfront.net/wp-content/uploads/sites/422/2018/04/UKKXIPXYVTEA.IMAGE_4.jpeg"
-          />
-        </div>
-      </div>
-      <div className="col-md-6 col-lg-4">
-        <div className="biocard">
-          <Bio
-            name="Renee Wise"
-            role="Lead Vision Therapist"
-            bio={[
-              "Renee has volunteered and worked with children in a variety of settings for 17 years. In addition to working with neuro-typical children, she also has extensive experience working with children who have mental, physical, and learning challenges such as: Autism Spectrum Disorder, Down's Syndrome, various Developmental Disorders, Cerebral Palsy, Muscular Dystrophy, Deafness, Hard of Hearing, and Blindness.",
-              'After 8 years of working and training as a Vision Therapist, Renee is proud to continue her growth and understanding of Optometric Vision Therapy.'
-            ]}
-            url="https://media.istockphoto.com/photos/female-portrait-icon-as-avatar-or-profile-picture-picture-id477333976?k=6&m=477333976&s=612x612&w=0&h=A5lI_2KJbVjyQpNsaCDWAR3jj-CLV1kqI6ObClYf4e4="
-          />
-        </div>
-      </div>
-      <div className="col-md-6 col-lg-4">
-        <div className="biocard">
-          <Bio
-            name="Paul Murray"
-            role="Operations Manager "
-            bio={[
-              'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate.',
-              'Voluptatum ducimus voluptates voluptas?'
-            ]}
-            url="https://media.istockphoto.com/photos/icon-of-a-businessman-avatar-or-profile-pic-picture-id474001892?k=6&m=474001892&s=612x612&w=0&h=6g0M3Q3HF8_uMQpYbkM9XAAoEDym7z9leencMcC4pxo="
-          />
-        </div>
-      </div>
+
+    <div className="biocard">
+      <Bio
+        name="Dr. Christina Murray, O.D."
+        bio={[
+          "Dr. Murray is originally from Columbus, Ohio, and is a proud graduate of The Ohio State University. She happily escaped the cold Ohio winters to attend optometry school at Nova Southeastern University. While in school, she discovered her passion for pediatrics and vision therapy. Dr. Murray's passion for helping children and adults reach their full potential through vision therapy has inspired her to Center for Better Learning. She is currently finishing up her board certification from the College of Optometrists in Vision Development.",
+          'When not seeing patients, Dr. Murray enjoys spending time with her husband, Paul, daughter, Mia, and two adorable golden retrievers, Sadie and Bentley. Her favorite activities include reading, baking, traveling.'
+        ]}
+        url="https://da4e1j5r7gw87.cloudfront.net/wp-content/uploads/sites/422/2018/04/UKKXIPXYVTEA.IMAGE_4.jpeg"
+      />
+    </div>
+
+    <div className="biocard">
+      <Bio
+        name="Renee Wise"
+        role="Lead Vision Therapist"
+        bio={[
+          "Renee has volunteered and worked with children in a variety of settings for 17 years. In addition to working with neuro-typical children, she also has extensive experience working with children who have mental, physical, and learning challenges such as: Autism Spectrum Disorder, Down's Syndrome, various Developmental Disorders, Cerebral Palsy, Muscular Dystrophy, Deafness, Hard of Hearing, and Blindness.",
+          'After 8 years of working and training as a Vision Therapist, Renee is proud to continue her growth and understanding of Optometric Vision Therapy.'
+        ]}
+        url="https://media.istockphoto.com/photos/female-portrait-icon-as-avatar-or-profile-picture-picture-id477333976?k=6&m=477333976&s=612x612&w=0&h=A5lI_2KJbVjyQpNsaCDWAR3jj-CLV1kqI6ObClYf4e4="
+      />
+    </div>
+
+    <div className="biocard">
+      <Bio
+        name="Paul Murray"
+        role="Operations Manager "
+        bio={[
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate.',
+          'Voluptatum ducimus voluptates voluptas?'
+        ]}
+        url="https://media.istockphoto.com/photos/icon-of-a-businessman-avatar-or-profile-pic-picture-id474001892?k=6&m=474001892&s=612x612&w=0&h=6g0M3Q3HF8_uMQpYbkM9XAAoEDym7z9leencMcC4pxo="
+      />
     </div>
   </main>
 );
