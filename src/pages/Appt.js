ff1357e76cba9f16cd45e6f2b3e9934bfa23ae71
AFarinacci <andreafarinacci@me.com>
2021-01-01 13:27:43 -0600

    add appt booking buttons

--- src/pages/Appt.js
+++ src/pages/Appt.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ApptForm from './../components/ApptForm';
 import Helmet from 'react-helmet';
+import { Button } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 function Appt() {
   return (
@@ -22,13 +25,35 @@ function Appt() {
             Schedule an Appointment
           </h1>
         </div>
-        <main className="container-fluid page paddingAround max800 effect8">
+        <div className="container-fluid" style={{ maxWidth: '900px' }}>
+          <h3>Book an appointment at the Coconut Creek Location</h3>
+          <Button
+            color="primary"
+            className="buttonPrimary"
+            href="https://outlook.office365.com/owa/calendar/CenterforBetterLearning@centerforbetterlearning.com/bookings/"
+          >
+            Book Appointment <FontAwesomeIcon icon={faArrowRight} />
+          </Button>
+          <br />
+          <br />
+          <h3>Book an appointment at the Boynton Beach Location</h3>
+          <Button
+            color="primary"
+            className="buttonPrimary"
+            href="https://outlook.office365.com/owa/calendar/CenterforBetterLearningBoyntonBeach@centerforbetterlearning.com/bookings/"
+          >
+            Book Appointment <FontAwesomeIcon icon={faArrowRight} />
+          </Button>
+          <br />
+          <br />
+        </div>
+        {/*  <main className="container-fluid page paddingAround max800 effect8">
           <div className="text-center" style={{ paddingBottom: '30px' }}>
             <h5>Please fill out the form below to request an appointment.</h5>
             <h5>We will get back to you shortly.</h5>
           </div>
           <ApptForm />
-        </main>
+        </main> */}
       </div>
     </div>
   );
