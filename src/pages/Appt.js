db1131bb5530593af9422c8bc7e631568411a5bd
AFarinacci <andreafarinacci@me.com>
2021-01-17 17:55:51 -0600

    add appt cols

--- src/pages/Appt.js
+++ src/pages/Appt.js
@@ -26,51 +26,38 @@ function Appt() {
           </h1>
         </div>
         <div className="container-fluid" style={{ maxWidth: '900px' }}>
-          <br />
-          <h3>Book an appointment at the Coconut Creek Location</h3>
-          <Button
-            color="primary"
-            className="buttonPrimary"
-            onClick={() =>
-              window.open(
-                'https://outlook.office365.com/owa/calendar/CenterforBetterLearning@centerforbetterlearning.com/bookings/',
-                '_blank'
-              )
-            }
-          >
-            Book Appointment <FontAwesomeIcon icon={faArrowRight} />
-          </Button>
-          <br />
-
-          <iframe
-            src="https://app.acuityscheduling.com/schedule.php?owner=21675469&location=8200%20S%20Jog%20Rd.%20Suite%20200%20Boynton%20Beach%2C%20FL%2033472"
-            title="Schedule Appointment"
-            width="100%"
-            height="800"
-            frameBorder="0"
-          ></iframe>
-          <script
-            src="https://embed.acuityscheduling.com/js/embed.js"
-            type="text/javascript"
-          ></script>
-
-          <br />
-          <br />
-          <h3>Book an appointment at the Boynton Beach Location</h3>
-          <Button
-            color="primary"
-            className="buttonPrimary"
-            onClick={() =>
-              window.open(
-                'https://outlook.office365.com/owa/calendar/CenterforBetterLearningBoyntonBeach@centerforbetterlearning.com/bookings/',
-                '_blank'
-              )
-            }
-          >
-            Book Appointment <FontAwesomeIcon icon={faArrowRight} />
-          </Button>
-          <br />
-          <br />
+          <Row bg-light>
+            <Col sm="12" md="6">
+              <h3>Book an appointment at the Coconut Creek Location</h3>
+              <Button
+                color="primary"
+                className="buttonPrimary"
+                onClick={() =>
+                  window.open(
+                    'https://app.acuityscheduling.com/schedule.php?owner=21675469&location=4171%20W%20Hillsboro%20Blvd.%20Suite%2013%20Coconut%20Creek%2C%20FL%2033073',
+                    '_blank'
+                  )
+                }
+              >
+                Book Appointment <FontAwesomeIcon icon={faArrowRight} />
+              </Button>
+            </Col>
+            <Col sm="12" md="6">
+              <h3>Book an appointment at the Boynton Beach Location</h3>
+              <Button
+                color="primary"
+                className="buttonPrimary"
+                onClick={() =>
+                  window.open(
+                    'https://app.acuityscheduling.com/schedule.php?owner=21675469&location=8200%20S%20Jog%20Rd.%20Suite%20200%20Boynton%20Beach%2C%20FL%2033472',
+                    '_blank'
+                  )
+                }
+              >
+                Book Appointment <FontAwesomeIcon icon={faArrowRight} />
+              </Button>
+            </Col>
+          </Row>
         </div>
         {/*  <main className="container-fluid page paddingAround max800 effect8">
           <div className="text-center" style={{ paddingBottom: '30px' }}>
