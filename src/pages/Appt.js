1dad3e6e3759548b3c3b3c0e0c8ae8bc988b6012
AFarinacci <andreafarinacci@me.com>
2021-01-17 18:03:51 -0600

    add appt cols with row col import

--- src/pages/Appt.js
+++ src/pages/Appt.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ApptForm from './../components/ApptForm';
 import Helmet from 'react-helmet';
-import { Button } from 'reactstrap';
+import { Row, Col, Button } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
